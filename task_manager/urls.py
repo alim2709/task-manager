c618918deeaeba4e8d5ba89b64ed8433b42e3bc2
Alim Selemetov <alimselemetow@gmail.com>
2023-07-12 16:27:47 +0300

    Add urls.py to manager and configure it with task_manager/urls.py

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("manager.urls", namespace="manager")),
     path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
