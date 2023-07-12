5517571e6fe151a2c5b063d8badb2a90c76e6c78
Alim Selemetov <alimselemetow@gmail.com>
2023-07-12 10:07:08 +0300

    Configure admin.py

--- manager/admin.py
+++ manager/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Worker, Task, Position, TaskType
 
-# Register your models here.
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("position",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "position",
+                    )
+                },
+            ),
+        )
+    )
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    search_fields = ("deadline", "name")
+    list_filter = ("task_type",)
+
+
+admin.site.register(Position)
+admin.site.register(TaskType)
