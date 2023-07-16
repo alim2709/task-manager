715ae54134104d0ddcf22efd77365a8d7c9bce85
Alim Selemetov <alimselemetow@gmail.com>
2023-07-16 21:51:55 +0300

    Add TaskForm to forms.py

--- manager/forms.py
+++ manager/forms.py
@@ -1,7 +1,10 @@
 from django import forms
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.admin.widgets import AdminDateWidget
+from django.forms import NumberInput
 
-from manager.models import Worker
+from manager.models import Worker, Task
 
 
 class SignUpForm(UserCreationForm):
@@ -13,3 +16,16 @@ class SignUpForm(UserCreationForm):
             "last_name",
         )
 
+
+class TaskForm(forms.ModelForm):
+
+    deadline = forms.DateTimeField(label="Date", required=True, widget=NumberInput(attrs={'type':'date'}))
+
+    assignees = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Task
+        fields = "__all__"
