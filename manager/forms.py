5080ec79d29d7393cc3b396245cb1bfc12113ba9
Alim Selemetov <alimselemetow@gmail.com>
2023-07-17 23:08:44 +0300

    Update forms.py

--- manager/forms.py
+++ manager/forms.py
@@ -2,9 +2,11 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.admin.widgets import AdminDateWidget
+from django.core.exceptions import ValidationError
 from django.forms import NumberInput
+from django.utils import timezone
 
-from manager.models import Worker, Task
+from manager.models import Worker, Task, Position
 
 
 class SignUpForm(UserCreationForm):
@@ -28,6 +30,17 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ["name", "description", "deadline", "priority", "task_type", "assignees"]
+
+    def clean_deadline(self):
+        deadline = self.cleaned_data.get("deadline")
+        now = timezone.now()
+
+        if deadline < now:
+            raise ValidationError(
+                "Deadline can't be earlier than current date"
+            )
+
+        return deadline
 
 
 class TaskSearchForm(forms.Form):
@@ -55,6 +68,27 @@ class WorkerSearchForm(forms.Form):
         label="",
         widget=forms.TextInput(attrs={"placeholder": "Search by username"})
     )
+
+
+class PositionForm(forms.ModelForm):
+    class Meta:
+        model = Position
+        fields = ("name",)
+
+    def clean_name(self):
+        return validate_position_name(self.cleaned_data["name"])
+
+
+def validate_position_name(position_name):
+    if not all(char.isalpha() or char.isspace() for char in position_name):
+        raise ValidationError(
+            "Characters in the position name can be only alphabetic or spaces."
+        )
+    if len(position_name) < 2:
+        raise ValidationError(
+            "The position name should have at least two letters."
+        )
+    return position_name
 
 
 class PositionSearchForm(forms.Form):
